5ea5fa367f3036e80604840a85634316a9931443
Ekta Verma <vermaekta9797@gmail.com>
2026-04-13 18:56:24 +0530

    Update ArrayList1.java

--- ArrayList1.java
+++ ArrayList1.java
@@ -1,17 +1,15 @@
+import java.util.Arrays;
 import java.util.ArrayList;
 
 public class ArrayList1 {
 //removing duplicates from arraylist
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        list.add(30);
-        list.add(20);
-        list.add(30);
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10,20,30,30,30));
         for(int i =0 ; i<list.size();i++){
             for(int j =i+1; j<list.size();j++){
                 if(list.get(i)==list.get(j)){
                     list.remove(j);
+                    j--;
                 }
             }
         }
