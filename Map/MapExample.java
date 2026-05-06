7fa4ac3519cff335b91beba03e7327109c5d7ee1
Ekta Verma <vermaekta9797@gmail.com>
2026-05-06 17:26:24 +0530

    Update MapExample.java

--- Map/MapExample.java
+++ Map/MapExample.java
@@ -11,46 +11,52 @@ public class MapExample {
 //All these functions are common in all three map classes.
         Map<String, Integer> map = new HashMap<>();
 
-        // 1. put() → insert data
+        // put() → insert data
         map.put("John", 90);
         map.put("Sam", 70);
         map.put("Kevin", 80);
         System.out.println(map);
-        // 2. put() → update value
+        
+        // put() → update value
 //If same key comes again → value gets updated
         map.put("John", 95);
         System.out.println(map);
-        // 3. get(key) → fetch value
+        
+        // get(key) → fetch value
         System.out.println("Marks of John: " + map.get("John"));
 
-        // 4. containsKey()
+        //returns value of the key if the key exists otherwise it returns defaultValue.
+        System.out.println(map.getOrDefault("Sam",0));
+        System.out.println(map.getOrDefault("Ekta", 0));
+
+        // containsKey()
         System.out.println("Has Sam? " + map.containsKey("Sam"));
 
-        // 5. containsValue()
+        // containsValue()
         System.out.println("Has value 80? " + map.containsValue(80));
 
-        // 6. size()
+        // size()
         System.out.println("Size: " + map.size());
 
-        // 7. isEmpty()
+        // isEmpty()
         System.out.println("Is empty? " + map.isEmpty());
 
-        // 8. keySet() → print all keys
+        // keySet() → print all keys
         for (String key : map.keySet()) {
             System.out.println(key);
         }
 
-        // 9. values() → print all values
+        // values() → print all values
         for (Integer val : map.values()) {
             System.out.println(val);
         }
 
-        // 10. entrySet() - BEST way to iterate
+        // entrySet() - BEST way to iterate
         for (Map.Entry<String, Integer> e : map.entrySet()) {
             System.out.println(e.getKey() + " -> " + e.getValue());
         }
 
-        // 11. remove()
+        // remove()
         map.remove("Sam");
 
         System.out.println("\nAfter removing Riya:");
